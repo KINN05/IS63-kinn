538b26eb228364292a51fe9801cf3ebddca7f564
KINN05 <rifqialkindi57@gmail.com>
2026-04-16 12:33:17 +0700

    16 april 2026

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MahasiswaController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,3 +10,10 @@ Route::get('/', function () {
 Route::get('/contoh1', function () {
     return view('lp3i');
 });
+
+Route::get('/master', function () {
+    return view('layout.master');
+});
+
+Route::get('mahasiswa', [MahasiswaController::class, 'index']);
+Route::get('mahasiswa/tambah', [MahasiswaController::class, 'index']);
